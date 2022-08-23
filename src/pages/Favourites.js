2d285b3821b2648154708d8046f8fd5f91004337
Charles Li <96878550+41CharlesLi@users.noreply.github.com>
2022-08-23 08:22:30 -0400

    restructured favourites page

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,24 +1,12 @@
 import Form from "../components/Form";
-import { useState } from "react";
 
 function Favourites() {
-    const [input, setInput] = useState("");
-
     return (
         <section className="Favourites">
-            <Form input={input} setInput={setInput} />
-            <div className="favBooksContainer">
-                <div className="indFavBooks">
-                    <div className="imgContainer">
-                        <img src="" alt="" />
-                    </div>
-                    <div className="ratingAndLikeContainer">
-                        <div className="starContainer">
-                            <p>rating</p>
-                        </div>
-                        <button>Like Button</button>
-                    </div>
-                </div>
+            <h2>Favourites Page</h2>
+            <div className="favouritesContainer">
+                <ul className="favouritesList"></ul>
+                <div className="paginationContainer"></div>
             </div>
         </section>
     );
