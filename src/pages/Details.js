d356a4694fd5498823def3b7fbef890406059522
Jingru Xu <jingruxu@Jingrus-Air.lan>
2022-08-24 21:29:42 -0400

    added some styles for Details.js page

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -29,7 +29,8 @@ function Details() {
         //buttons will probably need the book.id as an attribute so we can pass it a function to delete the book from the firebase repo
         <section className="details">
             <div className="wrapper">
-                <div className="imgContainer">
+
+                <div className="detailImgContainer">
                     <img
                         src={`https://books.google.com/books/publisher/content/images/frontcover/${bookId}?fife=w250-h400&source=gbs_api`}
                         alt={book.title}
@@ -38,6 +39,7 @@ function Details() {
                 <div className="detailContainer">
                     <h2>{book.title}</h2>
                     <div className="descriptionContainer">
+                    {book.categories && book.categories[0]}
                         {book.description && (
                             //dangerouslySetInnerHTML cleans out meta-data tags from your text and is a method that should only be used when you 100% trust the source of the information.
                             //behaves like innerHTML where malicious code can be injected within your site.
@@ -47,7 +49,7 @@ function Details() {
                                 }}
                             />
                         )}
-                        {book.categories && book.categories[0]}
+                        
                     </div>
 
                     <div className="buttonContainer">
@@ -55,13 +57,14 @@ function Details() {
                         <button>Delete</button>
                     </div>
 
-                    <button
-                        onClick={() => {
-                            window.history.back();
-                        }}
-                    >
-                        Back
+                    <button className="backButton"
+                            onClick={() => {
+                                window.history.back();
+                            }}
+                        >
+                            Back
                     </button>
+
                 </div>
             </div>
         </section>
